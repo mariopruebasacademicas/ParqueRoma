3c06b4adb689ff37114ae7308bec3052501548d3
Mario Jimenez <lenovomario69@gmail.com>
2025-10-22 23:44:09 +0200

    Modificación pantalla principal

--- demo/src/main/java/com/parqueroma/parque_roma-front/src/App.jsx
+++ demo/src/main/java/com/parqueroma/parque_roma-front/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          CDE Parque Roma - DataBase
         </p>
         <a
           className="App-link"
